a384e1d0470767180b16e6cb330fa392866c458d
volker-baecker <volker.baecker@mri.cnrs.fr>
2023-09-13 00:22:48 +0200

    started the synthetic 3D spot generator

---
+++ spot_generator.py
@@ -0,0 +1,34 @@
+import random
+
+class SpotGenerator:
+
+
+    def __init__(self):
+        self.width = 512
+        self.height = 512
+        self.depth = 64
+        self.bitDepth = 16
+        self.mask = None
+        self.numberOfSamples = 1000
+        self.samples
+        
+    def sampleUniformRandomPoints(self):
+        if mask:
+            self.sampleUniformRandomPointsInMask()
+        else:
+            self.sampleUniformRandomPointsInImage()
+            
+           
+    def sampleUniformRandomPointsInMask(self):
+        pass
+        
+        
+    def sampleUniformRandomPointsInImage(self):
+        random.randrange(self.width * self.height * self.depth)
+        
+    
+    def indicesToCoordinates(self, indices):
+        self.pass
+        
+    
+    
