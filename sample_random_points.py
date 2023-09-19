4ceef9efd8f0c779fca7fc2a04bda38f4646af6e
volker-baecker <volker.baecker@mri.cnrs.fr>
2023-09-19 18:01:39 +0200

    started the first script

---
+++ sample_random_points.py
@@ -0,0 +1,186 @@
+###############################################################################################
+##
+## sample_random_points.py
+##
+## Answers a number of random points in the image or mask with a uniform distribution
+## 
+## (c) 2023 INSERM
+##
+## written by Volker Baecker at the MRI-Center for Image Analysis (MRI-CIA - https://www.mri.cnrs.fr/en/data-analysis.html)
+##
+## sample_random_points.py is free software under the MIT license.
+## 
+## MIT License
+##
+## Copyright (c) 2023 INSERM
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+## 
+################################################################################################
+
+import os
+from ij import IJ
+from ij import Prefs
+from ij import WindowManager
+from ij.gui import GenericDialog
+from fr.cnrs.mri.cialib.generator import SpotGenerator
+from fr.cnrs.mri.cialib.imaging import Microscope
+
+
+URL = "https://github.com/MontpellierRessourcesImagerie/point-pattern-analysis/wiki/3D_Synthetic_Spots";
+
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+IMAGE_DEPTH = 64 
+IMAGE_TYPE = "16-bit"
+IMAGE_TYPES = ["8-bit", "16-bit", "32-bit"]
+VOXEL_SIZE_XY = 0.7 
+VOXEL_SIZE_Z = 5
+UNIT = chr(181) + "m"
+MASK = None
+NUMBER_OF_SAMPLES = 1000
+SAVE_OPTIONS = True
+
+
+def main():
+    optionsOnly = Prefs.get("mri.options.only", "false")
+    if  not showDialog():
+        return
+    if optionsOnly=="true":
+        return
+    gen = SpotGenerator()
+    if MASK:
+        gen.setMask(IJ.getImage(MASK))
+    gen.width = IMAGE_WIDTH
+    gen.height = IMAGE_HEIGHT
+    gen.depth = IMAGE_DEPTH
+    if IMAGE_TYPE == "8-bit":
+        gen.bitDepth = 8
+    if IMAGE_TYPE == "16-bit":
+        gen.bitDepth = 16
+    if IMAGE_TYPE == "32-bit":
+        gen.bitDepth = 32
+    gen.calibration.pixelWidth = VOXEL_SIZE_XY
+    gen.calibration.pixelHeight = VOXEL_SIZE_XY
+    gen.calibration.pixelDepth = VOXEL_SIZE_Z
+    gen.calibration.setUnit(UNIT)
+    gen.numberOfSamples = NUMBER_OF_SAMPLES
+    gen.sampleUniformRandomPoints()
+    gen.createGroundTruthImage()
+    gen.groundTruthImage.show()
+    table = gen.getGroundTruthTable()
+    table.show("Random Points Uniform Distribution")
+    
+    
+def showDialog():
+    global IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_DEPTH, IMAGE_TYPE, VOXEL_SIZE_XY, VOXEL_SIZE_Z, UNIT, MASK, NUMBER_OF_SAMPLES, SAVE_OPTIONS
+    
+    images = WindowManager.getImageTitles()
+    if  os.path.exists(getOptionsPath()):
+        loadOptions()
+    gd = GenericDialog("Sample Random Points Options"); 
+    gd.addNumericField("Width Of Image: ", IMAGE_WIDTH)
+    gd.addNumericField("Height Of Image: ", IMAGE_HEIGHT)
+    gd.addNumericField("Depth Of Image: ", IMAGE_DEPTH)
+    gd.addChoice("Type of Image: ", IMAGE_TYPES, IMAGE_TYPE)
+    gd.addNumericField("XY-size of voxel: ", VOXEL_SIZE_XY)
+    gd.addNumericField("Z-size of Voxel: ", VOXEL_SIZE_Z)
+    gd.addStringField("Unit: ", UNIT)
+    gd.addNumericField("Number Of Samples: ", NUMBER_OF_SAMPLES)
+    if images:
+        gd.addChoice("Mask: ", images, MASK)
+    gd.addCheckbox("Save Options", SAVE_OPTIONS)
+    gd.addHelp(URL)
+    gd.showDialog()
+    if gd.wasCanceled():
+        return False
+    IMAGE_WIDTH = int(gd.getNextNumber())
+    IMAGE_HEIGHT = int(gd.getNextNumber())
+    IMAGE_DEPTH = int(gd.getNextNumber())
+    IMAGE_TYPE = gd.getNextChoice()
+    VOXEL_SIZE_XY = gd.getNextNumber()
+    VOXEL_SIZE_Z = gd.getNextNumber()
+    UNIT = gd.getNextString()
+    NUMBER_OF_SAMPLES = int(gd.getNextNumber())
+    if images:
+        MASK = gd.getNextChoice()
+    SAVE_OPTIONS = gd.getNextBoolean()
+    if SAVE_OPTIONS:
+        saveOptions()
+    return True
+  
+  
+def getOptionsPath():
+    pluginsPath = IJ.getDirectory("plugins")
+    optionsPath = pluginsPath + "3D_Synthetic_Spots/3dsrp-options.txt"
+    return optionsPath
+ 
+
+def getOptionsString():
+    optionsString = ""
+    optionsString = optionsString + " width=" + str(IMAGE_WIDTH) 
+    optionsString = optionsString + " height=" + str(IMAGE_HEIGHT) 
+    optionsString = optionsString + " depth=" + str(IMAGE_DEPTH) 
+    optionsString = optionsString + " type=" + str(IMAGE_TYPE) 
+    optionsString = optionsString + " xy-size=" + str(VOXEL_SIZE_XY) 
+    optionsString = optionsString + " z-size=" + str(VOXEL_SIZE_Z) 
+    optionsString = optionsString + u" unit=" + UNIT
+    optionsString = optionsString + " number=" + str(NUMBER_OF_SAMPLES) 
+    
+    return optionsString
+    
+    
+def loadOptions(): 
+    global IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_DEPTH, IMAGE_TYPE, VOXEL_SIZE_XY, VOXEL_SIZE_Z, UNIT, MASK, NUMBER_OF_SAMPLES
+    
+    optionsPath = getOptionsPath()
+    optionsString = IJ.openAsString(optionsPath)
+    optionsString = optionsString.replace("\n", "")
+    options = optionsString.split(" ")
+    for option in options:
+        parts = option.split("=")
+        key = parts[0]
+        value = ""
+        if "=" in option:
+            value = parts[1]
+        if key=="width":
+            IMAGE_WIDTH = int(value)
+        if key=="height":
+            IMAGE_HEIGHT = int(value)
+        if key=="depth":
+            IMAGE_DEPTH = int(value)        
+        if key=="type":
+            IMAGE_TYPE = value
+        if key=="xy-size":
+            VOXEL_SIZE_XY = float(value)
+        if key=="z-size":
+            VOXEL_SIZE_z = float(value)
+        if key=="unit":
+            UNIT = value
+        if key=="number":
+            NUMBER_OF_SAMPLES = int(value)    
+   
+
+def saveOptions():
+    optionsString = getOptionsString()
+    optionsPath = getOptionsPath()
+    IJ.saveString(optionsString, getOptionsPath())
+    
+    
+main()   
