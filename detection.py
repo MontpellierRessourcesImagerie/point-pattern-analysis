08a8ec63394219deafabf5ce78a54651728db35a
volker-baecker <volker.baecker@mri.cnrs.fr>
2023-10-22 02:49:21 +0200

    detection is working, started 3D overlay

--- detection.py
+++ detection.py
@@ -36,12 +36,18 @@
 
 from __future__ import division
 import math
+from ij import IJ
+from ij import WindowManager
 from ij.plugin.filter import PlugInFilterRunner
 from ij.plugin.filter import ExtendedPlugInFilter
 from ij.process import ImageConverter
 from ij.gui import DialogListener
+from ij.process import StackStatistics
+from ij.process import StackProcessor
 from imagescience.feature import Laplacian
 from imagescience.image import Image
+from mcib3d.image3d.processing import MaximaFinder
+from mcib3d.image3d import ImageHandler
 
 
 class SpotDetectionPlugInFilter(ExtendedPlugInFilter, DialogListener):
@@ -77,20 +83,53 @@ class SpotDetector():
 
 
     def __init__(self):
-        self.spotDiameterXY = 0.5 
-        self.spotDiameterZ = 0.5
-        self.prominence = 0.4
+        self.spotDiameterXY = 0.65 
+        self.spotDiameterZ = 2.25
+        self.prominence = 0.15
+        self.threshold = 0.35    
+        self.spots = []
+        self.spotImage = None
+        IJ.run("FeatureJ Options", "isotropic progress log")
         
    
-    def run(self, image):
-        logFilter = Laplacian()
-        sigmaXY = (self.spotDiameterXY / 2.0) / math.sqrt(3)
+    def run(self, inputImage):        
+        image = inputImage.crop("stack")
+        sigmaXY = (self.spotDiameterXY / 2) / math.sqrt(2)
+        squaredSigmaXY = sigmaXY * sigmaXY
+        spotRadiusPixelXY = image.getCalibration().getRawX(self.spotDiameterXY / 2.0)
+        spotRadiusPixelZ = image.getCalibration().getRawZ(self.spotDiameterZ / 2.0)
         doScaling = ImageConverter.getDoScaling()
         ImageConverter.setDoScaling(False)
-        imageScienceImage = Image.wrap(image)
-        logImage = logFilter.run(imageScienceImage, sigmaXY)
-        logImage = logImage.imageplus()
-        logImage.show()
+        print(sigmaXY)
+        print(spotRadiusPixelXY)
+        print(spotRadiusPixelZ)
+        logImage = LoGFilter(sigmaXY).run(image)
+        IJ.run(logImage, "Multiply...", "value=" + str(squaredSigmaXY) + " stack")
+        stats = StackStatistics(logImage)
+        IJ.run(logImage, "Subtract...", "value=" + str(stats.min) + " stack")
+        logImage.getProcessor().setMinAndMax(0, stats.max - stats.min)
+        StackProcessor(logImage.getStack()).invert()
+        stats = StackStatistics(logImage)
+        thresholded = ImageHandler.wrap(logImage.duplicate())
+        thresholded.thresholdCut(self.threshold, False, True)
+        maxFinder = MaximaFinder(thresholded, spotRadiusPixelXY, spotRadiusPixelZ, self.prominence)
+        self.spotImage = maxFinder.getImagePeaks().getImagePlus()  
+        self.spots = list(maxFinder.getListPeaks())
         ImageConverter.setDoScaling(doScaling)
+       
+   
+   
+class LoGFilter():
     
+    
+    def __init__(self, sigma):
+        self.filter = Laplacian()
+        self.sigma = sigma
+    
+    
+    def run(self, image):
+        imageScienceImage = Image.wrap(image.duplicate())
+        logImage = self.filter.run(imageScienceImage, self.sigma)
+        return logImage.imageplus()
+        
     
--- workspace.py
+++ workspace.py
@@ -1,6 +1,34 @@
 from fr.cnrs.mri.cialib.detection import SpotDetector
 from ij import IJ
+from ij import ImagePlus
+from ij.gui import ImageRoi
+from ij.gui import Overlay
+from ij.plugin import LutLoader
+from ij.process import LUT 
+from ij.process import StackStatistics
+from inra.ijpb.binary.conncomp import FloodFillComponentsLabeling3D
+from inra.ijpb.morphology.strel import BallStrel
 
+lutName = "glasbey on dark"
+lut = LUT(LutLoader.getLut(lutName ), 0, 255)
 detector = SpotDetector()
 image = IJ.getImage()
+roi = image.getRoi()
+print(roi)
 detector.run(image)
+labels = detector.spotImage.duplicate()
+floodFiller = FloodFillComponentsLabeling3D(6, 16)
+res = floodFiller.computeResult(labels.getStack())
+labelsStack = res.labelMap
+strel = BallStrel.fromRadius(2)
+labelsStack = strel.dilation(labelsStack)
+resImage = ImagePlus("labels", labelsStack)
+resImage.getChannelProcessor().setLut(lut)
+resImage.resetDisplayRange()
+rois = []
+for i in range(1, labelsStack.getSize()+1):
+    roi = ImageRoi(roi.getBounds().x, roi.getBounds().y, labelsStack.getProcessor(i))
+    rois.append(roi)
+overlay = Overlay.createStackOverlay(rois)
+image.setOverlay(overlay)
+
